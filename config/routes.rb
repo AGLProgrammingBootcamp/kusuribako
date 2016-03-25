64de3968d159552cfaa71cdc63188a25c3a0eb6b
kohei_k <k_kai@es.dis.titech.ac.jp>
2016-03-25 07:31:44 +0000

    mail_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'notes/new'
+
   get 'inquiry' => 'inquiry#index'              # 入力画面
   post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
   post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
