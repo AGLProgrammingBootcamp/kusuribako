eb47334145f22b24b76f864dbf24af9e992dbd6d
kohei_k <k_kai@es.dis.titech.ac.jp>
2016-03-26 05:24:26 +0000

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,2 @@
 Rails.application.routes.draw do
-  get 'medicines/new'=>'medicines#new'
-  post 'medicines'=>'medicines#create'
-  get 'medicines' =>'medicines#index'
-  
-  get 'inquiry' => 'inquiry#index'              # 入力画面
-  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
-  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
-
 end
